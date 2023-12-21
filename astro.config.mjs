2de85a06c2bd75f903745f2e0488d63fb7dc9853
Maximilian Kaske <maximilian@kaske.org>
2023-12-21 17:11:07 +0100

    wip: add hover card and improve design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,25 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
+import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   output: "server",
   adapter: cloudflare({
     runtime: {
-      mode: 'local',
-      type: 'pages',
+      mode: "local",
+      type: "pages",
       bindings: {
         // example of a var binding (environment variable)
-        "API_KEY": {
+        API_KEY: {
           type: "secret",
-        }
-      }
-    }
+        },
+      },
+    },
   }),
-
 });
-
 
 // // https://astro.build/config
 // export default defineConfig({
