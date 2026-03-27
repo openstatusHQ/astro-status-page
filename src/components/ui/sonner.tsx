b03af4ea4091cdb4c1477c33db10f3d47a93e27a
Moulik Aggarwal <qwertymoulik@gmail.com>
2026-03-28 02:08:12 +0530

    Updated packages
    Signed-off-by: Moulik Aggarwal <qwertymoulik@gmail.com>

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,13 +1,10 @@
-import { useTheme } from "next-themes"
 import { Toaster as Sonner, type ToasterProps } from "sonner"
 import { CircleCheckIcon, InfoIcon, TriangleAlertIcon, OctagonXIcon, Loader2Icon } from "lucide-react"
 
-const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
-
+const Toaster = ({ theme = "light", ...props }: ToasterProps) => {
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
+      theme={theme}
       className="toaster group"
       icons={{
         success: (
